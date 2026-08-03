a9e5ee26bd708b09446eb0779d52c017bc65d298
Krish Pai <krish.pai@outlook.com>
2026-08-02 23:07:28 -0400

    next commit

--- src/components/SearchResultsView.tsx
+++ src/components/SearchResultsView.tsx
@@ -112,7 +112,7 @@ const CustomFooter = (props: CustomFooterProps) => {
         onPageChange={onPageChange ?? (() => { })}
         rowsPerPage={pageSize}
         onRowsPerPageChange={onPageSizeChange}
-        rowsPerPageOptions={[10, 15, 25]}
+        rowsPerPageOptions={[10, 15, 25, 50]}
         sx={{
           border: 'none',
           '& .MuiTablePagination-toolbar': {
